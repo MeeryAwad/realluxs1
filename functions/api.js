6b8e2e3ab8356630a655eba29d4a2c51a30f7a5a
MeeryAwad <meeryawad19@hotmail.com>
2023-12-11 17:08:09 +0400

    all9

--- functions/api.js
+++ functions/api.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
 const corsOption = {
-    origin: ['http://localhost:3000'],
+   
     credentials: true,
     methods: ["get", "post", "put", "delete", "patch"],
 }
